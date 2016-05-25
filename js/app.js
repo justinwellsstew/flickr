a3e7bbcbce4dbdac54e8ff313d3a0fd33542dfcd
Justin Stewart <stewartjustin3@gmail.com>
2016-05-24 20:30:21 -0400

    added wait for searching

--- js/app.js
+++ js/app.js
@@ -1,30 +1,37 @@
 angular.module('flickrApp', [])
-.controller('flickrCtrl', function($scope, $http){
+.controller('flickrCtrl', function($scope, $http, $q, $timeout){
 
-	
 
     $scope.submitted = false;
+    $scope.search = false;
 
 	$scope.submit = function(){
-		$scope.submitted = true;
+	    	var url = "https://api.flickr.com/services/rest";
+			var params = {
+			    method: 'flickr.photos.search',
+			    api_key: 'c79ebccb4daf5d50cb6963bd044ebddf',
+			    tags: $scope.searchInput,
+			    format: 'json',
+			    nojsoncallback: 1
+	    	}
+		    $http({
+		    	url : url,
+		    	params : params,
 
-		var url = "https://api.flickr.com/services/rest";
-		var params = {
-		    method: 'flickr.photos.search',
-		    api_key: 'c79ebccb4daf5d50cb6963bd044ebddf',
-		    tags: $scope.searchInput,
-		    format: 'json',
-		    nojsoncallback: 1
-	    }
-	    $http({
-	    	url : url,
-	    	params : params,
-	    }).success(function(results){
-	    	$scope.photos = results.photos.photo;
-	    	console.log ($scope.photos);
-	     });
-	    
-
-		console.log('form submitted');
+		     }).success(function(results){
+		    	$scope.search = true;
+		    	$scope.photos = results.photos.photo;
+		    }).then(wait)
+		    .then(function(results){
+		    	$scope.search = false;
+		    	$scope.submitted = true;
+		});
+			function wait() {
+		    return $q(function(resolve, reject){
+		      $timeout(function() {
+		        resolve();
+		      }, 2000);
+		  })
+		}
 	}
-})
+});
